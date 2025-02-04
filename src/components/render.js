58f811edd3d051be3f06e5a82a94191ae0e65385
ajan890 <66571533+ajan890@users.noreply.github.com>
2025-02-03 20:29:33 -0800

    spinning car :3

--- src/components/render.js
+++ src/components/render.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
 
 function generateScene(mountRef) {
     // Set up the scene, camera, and renderer
@@ -12,16 +13,40 @@ function generateScene(mountRef) {
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     mountRef.current.appendChild(renderer.domElement);
+    var carModel;
+
+    // light sources
+    const ambientLight = new THREE.AmbientLight(0x404040);
+    scene.add(ambientLight);
+    
+    var lightTop = new THREE.PointLight(0xffffff, 75, 0);
+    lightTop.position.set(5, 5, 5);
+    scene.add(lightTop)
 
     // Create a rotating cube
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x42069f });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    // const geometry = new THREE.BoxGeometry();
+    // const material = new THREE.MeshPhongMaterial({
+    //     color: 0x69f420 ,
+    //     shininess: 100,
+    //     specular: 0xffffff
+    //     });
+    // const cube = new THREE.Mesh(geometry, material);
+    // scene.add(cube);
 
+    const loader = new OBJLoader();
+    loader.load('./models/Aeroshell_16.obj', (object) => {
+        object.scale.set(0.001, 0.001, 0.001); // Adjust scale if necessary
+        object.position.set(0, 0, 0); // Adjust position if needed
+        scene.add(object);
+        carModel = object;
+    }, undefined, (error) => {
+        console.error('Error loading model:', error);
+    });
+    
+    // camera parameters
     camera.position.x = 0;  // left/right
     camera.position.y = 5;  // up/down
-    camera.position.z = 5;  // forward/backward
+    camera.position.z = 8;  // forward/backward
     
     camera.rotation.x = -Math.PI / 6  // up/down  (angles in radians)
     camera.rotation.y = 0  // sides   (angles in radians)
@@ -31,7 +56,10 @@ function generateScene(mountRef) {
     const animate = () => {
         requestAnimationFrame(animate);
         // cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        // cube.rotation.y += 0.01;
+        if (carModel) {
+            carModel.rotation.y += 0.01;
+        }
         renderer.render(scene, camera);
     };
     animate();
