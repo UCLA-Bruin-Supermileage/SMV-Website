9749eb5ce5f2e29ce2e224dc1a77e488b04fcf95
ShawnL57 <110152943+ShawnL57@users.noreply.github.com>
2025-02-10 19:15:33 -0800

    Added hyper links of: team-view, documentation, and dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,16 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className = "text-2xl" > 
-      <div className ="grid grid-cols-2 grid-flow-row gap-4"> 
+      <div className ="grid grid-cols-1 grid-flow-row gap-4"> 
         <div>
-          Go to 
-          <Link className="text-2xl" className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href = "/dashboard"> DashBoard</Link>
-          !
+          Go to <Link className="text-2xl" className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href = "/dashboard">DashBoard</Link>!
         </div>
 
         <div>
-          Go to 
-          <Link className = "text-2xl" className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href = "/team-view"> team-view</Link>
-          !
+          Go to <Link className = "text-2xl" className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href = "/team-view">team-view</Link>!
+        </div> 
+        <div>
+          Go to <Link className = "text-2xl" className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href = "https://smv.seas.ucla.edu/info/">Documentation</Link>!
         </div> 
       </div> 
     </div>
