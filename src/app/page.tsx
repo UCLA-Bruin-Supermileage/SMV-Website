7d7ab08f80eb63b819492adfe65fadd40e83a46d
Howard Zhu <67174263+Howard-Z@users.noreply.github.com>
2025-03-09 04:28:13 -0700

    build version of 3js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from 'next/link';
 import Scene from './render/page';
 
@@ -7,14 +6,14 @@ export default function Home() {
     <div className = "text-2xl" > 
       <div className ="grid grid-cols-1 grid-flow-row gap-4"> 
         <div>
-          Go to <Link className="text-2xl" className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href = "/dashboard">DashBoard</Link>!
+          Go to <Link className="dashboard text-2xl underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href = "/dashboard">DashBoard</Link>!
         </div>
 
         <div>
-          Go to <Link className = "text-2xl" className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href = "/team-view">team-view</Link>!
+          Go to <Link className="team text-2xl underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href = "/team-view">team-view</Link>!
         </div> 
         <div>
-          Go to <Link className = "text-2xl" className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href = "https://smv.seas.ucla.edu/info/">Documentation</Link>!
+          Go to <Link className="docs text-2xl underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href = "https://smv.seas.ucla.edu/info/">Documentation</Link>!
         </div> 
         <div className="render-box">
             <Scene/>
